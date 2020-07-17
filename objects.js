586ce8289216670ce6c7331e6d6467807f997797
Joseph Renolayan <jrenolayan@gmail.com>
2020-07-17 13:37:24 -0700

    promises under the hood

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ const fn = {}
 
 // Preflight
 const mergeArrays = (arr1, arr2) => {
-  return [...arr1,...arr2]
+  return [...arr1, ...arr2]
 }
 
 const firstLongerThan = (arr, num) => {
@@ -12,17 +12,31 @@ const firstLongerThan = (arr, num) => {
 
 const getReturnValues = (arr) => arr.map(e => e())
 
-const subArray = (num, arr=[]) => {
+const subArray = (num, arr = []) => {
   if (num === arr.length) return arr
   arr[arr.length] = 0
   return subArray(num, arr)
 }
 
-const zeroSquare = (num, res=[]) => {
+const zeroSquare = (num, res = []) => {
   if (num === res.length) return res
   res[res.length] = subArray(num)
   return zeroSquare(num, res)
 }
+
+// Promises under the hood
+
+// const prepareStage = {
+//   then: (func) => {
+//     func()
+//     return prepareStage
+//   }
+// }
+
+// const performMagic = () => console.log('Abra')
+
+// prepareStage.then(performMagic).then(performMagic).then(performMagic)
+
 
 fn.mergeArrays = mergeArrays
 fn.firstLongerThan = firstLongerThan
